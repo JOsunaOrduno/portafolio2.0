d1098f7d158c6a74aeded068d4884cef1e196b2e
Ladingo2000 <82851676+JOsunaOrduno@users.noreply.github.com>
2023-04-19 12:43:44 -0600

    Videos arreglados
    Videos Corregidos, se detienen cuando cambias y se puede cerrar la descripcion

--- index.php
+++ index.php
@@ -182,34 +182,69 @@
             <div class="slideshow-container">
 
                 <div class="mySlides fade">
-                    <div class="video" id="video1"></div>
+                    <div class="video" id="Etiquetado"></div>
                     <div class="desc">
-                        <h1>TITULO 1</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            At cum porro, delectus quaerat, similique quo nam dolorem facere accusantium beatae 
-                            nihil quam enim natus doloribus vel eveniet dignissimos ullam molestiae?
+                        <span class="closeD">&times;</span>
+                        <h1>Etiquetado</h1>
+                        <p>
+                            El cliente necesita tener control sobre el producto terminado para 
+                            llevar un inventario más eficiente y evitar duplicado de pedidos. 
+                            Por lo que con el desarrollo de un software especializado que 
+                            lleva enlace entre un escáner de código de barras, 
+                            se resuelven las situaciones mencionadas del cliente.
                         </p>
                     </div>
                     <!--<div class="text">Caption Text</div>-->
                 </div>
                 
                 <div class="mySlides fade">
-                    <div class="video" id="video2"></div>
+                    <div class="video" id="Inventario"></div>
                     <div class="desc">
-                        <h1>TITULO 2</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            At cum porro, delectus quaerat, similique quo nam dolorem facere accusantium beatae 
-                            nihil quam enim natus doloribus vel eveniet dignissimos ullam molestiae?
+                        <span class="closeD">&times;</span>
+                        <h1>Inventario</h1>
+                        <p>
+                            Se crea software especializado para la emisión de etiquetas para 
+                            el control de inventario, estas llevarán datos del producto así como nombre 
+                            del responsable en turno de la generación de las mismas, la etiqueta se pega 
+                            en el paquete del producto y lleva un código de barras que ayuda a identificarlo.
                         </p>
                     </div>
                 </div>
                 
                 <div class="mySlides fade">
-                    <div class="video" id="video3"></div>
+                    <div class="video" id="GPS"></div>
+                    <div class="desc">
+                        <span class="closeD">&times;</span>
+                        <h1>GPS</h1>
+                        <p>
+                            El cliente necesita saber que los vehículos que asignan
+                             para sus funciones comerciales no sean utilizados de 
+                             manera incorrecta, además de conocer la eficiencia de los 
+                             servicios realizados con respecto al gasto que esto implica, 
+                             ventas realizadas y control de las ubicaciones de sus equipos de transporte.
+
+                        </p>
+                    </div>
+                </div>
+
+                <div class="mySlides fade">
+                    <div class="video" id="Interface"></div>
+                    <div class="desc">
+                        <span class="closeD">&times;</span>
+                        <h1>Interface</h1>
+                        <p>
+                            El cliente requiere una interfaz para el control de ventas 
+                            de boletos, busca conocer el tipo de cliente que frecuenta 
+                            mayormente el museo, clasificando entre, hombres, mujeres y 
+                            niños de distintas edades, cantidades y suma para finalmente 
+                            emitir los tickets de acceso al museo. La interfaz se hace con 
+                            requerimientos de diseño a medida del cliente.
+                        </p>
+                    </div>
                 </div>
                 
-                <a class="prev" onclick="plusSlides(-1)">❮</a>
-                <a class="next" onclick="plusSlides(1)">❯</a>
+                <a class="prev" id="prev" onclick="plusSlides(-1)">❮</a>
+                <a class="next" id="next" onclick="plusSlides(1)">❯</a>
                 
             </div>
 
@@ -219,9 +254,10 @@
                 <span class="dot" onclick="currentSlide(1)"></span> 
                 <span class="dot" onclick="currentSlide(2)"></span> 
                 <span class="dot" onclick="currentSlide(3)"></span> 
+                <span class="dot" onclick="currentSlide(4)"></span> 
             </div>
-            <script src="JS/vids.js"></script>
-        </section>  
+        </section>
+
             <!-- Contenedor de las tarjetas -->
         <div class="tarjetas">
             <div class = "container2">
@@ -284,5 +320,6 @@
         div.classList.toggle("abajoServ",window.scrollY>600);
     })
 </script>
+<script src="JS/vids.js"></script>
 
 </html>
