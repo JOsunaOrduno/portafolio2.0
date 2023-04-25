34bae8b0949cbbeac65ff4c3aa99adbbcd5195b1
Ladingo2000 <82851676+JOsunaOrduno@users.noreply.github.com>
2023-04-25 00:52:30 -0600

    Cambios JP reunion viernes 21
    Centrado anadido mapa, formateado

--- index.php
+++ index.php
@@ -186,52 +186,6 @@
             <div class="slideshow-container">
 
                 <div class="mySlides fade">
-                    <div class="video" id="Etiquetado"></div>
-                    <div class="desc">
-                        <span class="closeD">&times;</span>
-                        <h1>Etiquetado</h1>
-                        <p>
-                            El cliente necesita tener control sobre el producto terminado para 
-                            llevar un inventario más eficiente y evitar duplicado de pedidos. 
-                            Por lo que con el desarrollo de un software especializado que 
-                            lleva enlace entre un escáner de código de barras, 
-                            se resuelven las situaciones mencionadas del cliente.
-                        </p>
-                    </div>
-                    <!--<div class="text">Caption Text</div>-->
-                </div>
-                
-                <div class="mySlides fade">
-                    <div class="video" id="Inventario"></div>
-                    <div class="desc">
-                        <span class="closeD">&times;</span>
-                        <h1>Inventario</h1>
-                        <p>
-                            Se crea software especializado para la emisión de etiquetas para 
-                            el control de inventario, estas llevarán datos del producto así como nombre 
-                            del responsable en turno de la generación de las mismas, la etiqueta se pega 
-                            en el paquete del producto y lleva un código de barras que ayuda a identificarlo.
-                        </p>
-                    </div>
-                </div>
-                
-                <div class="mySlides fade">
-                    <div class="video" id="GPS"></div>
-                    <div class="desc">
-                        <span class="closeD">&times;</span>
-                        <h1>GPS</h1>
-                        <p>
-                            El cliente necesita saber que los vehículos que asignan
-                             para sus funciones comerciales no sean utilizados de 
-                             manera incorrecta, además de conocer la eficiencia de los 
-                             servicios realizados con respecto al gasto que esto implica, 
-                             ventas realizadas y control de las ubicaciones de sus equipos de transporte.
-
-                        </p>
-                    </div>
-                </div>
-
-                <div class="mySlides fade">
                     <div class="video" id="Interface"></div>
                     <div class="desc">
                         <span class="closeD">&times;</span>
@@ -245,11 +199,57 @@
                             requerimientos de diseño a medida del cliente.
                         </p>
                     </div>
+                    <!--<div class="text">Caption Text</div>-->
                 </div>
-                
+
+                <div class="mySlides fade">
+                    <div class="video" id="Inventario"></div>
+                    <div class="desc">
+                        <span class="closeD">&times;</span>
+                        <h1>Inventario</h1>
+                        <p>
+                            Se crea software especializado para la emisión de etiquetas para 
+                            el control de inventario, estas llevarán datos del producto así como nombre 
+                            del responsable en turno de la generación de las mismas, la etiqueta se pega 
+                            en el paquete del producto y lleva un código de barras que ayuda a identificarlo.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="mySlides fade">
+                    <div class="video" id="GPS"></div>
+                    <div class="desc">
+                        <span class="closeD">&times;</span>
+                        <h1>GPS</h1>
+                        <p>
+                            El cliente necesita saber que los vehículos que asignan
+                            para sus funciones comerciales no sean utilizados de 
+                            manera incorrecta, además de conocer la eficiencia de los 
+                            servicios realizados con respecto al gasto que esto implica, 
+                            ventas realizadas y control de las ubicaciones de sus equipos de transporte.
+
+                        </p>
+                    </div>
+                </div>
+
+                <div class="mySlides fade">
+                    <div class="video" id="Etiquetado"></div>
+                    <div class="desc">
+                        <span class="closeD">&times;</span>
+                        <h1>Etiquetado</h1>
+                        <p>      
+                            El cliente necesita tener control sobre el producto terminado para 
+                            llevar un inventario más eficiente y evitar duplicado de pedidos. 
+                            Por lo que con el desarrollo de un software especializado que 
+                            lleva enlace entre un escáner de código de barras, 
+                            se resuelven las situaciones mencionadas del cliente.
+                        </p>
+                    </div>
+                </div>
+
                 <a class="prev" id="prev" onclick="plusSlides(-1)">❮</a>
                 <a class="next" id="next" onclick="plusSlides(1)">❯</a>
-                
+
             </div>
 
             <br>
@@ -267,6 +267,7 @@
             <div class = "container2">
                 <!--Primer tarjeta-->
                 <div class = "card">
+                    <h1>MISION</h1>
                     <!--Contenido de la tarjeta, se puede cambiar el texto o la imagen sin problema -->
                     <div class="content">
                         <h2>Mision</h2>
@@ -280,6 +281,7 @@
                 </div>
         
                 <div class = "card">
+                    <h1>VISION</h1>
                     <div class="content">
                         <h2>Vision</h2>
                         <p>
@@ -296,15 +298,18 @@
             </div>
         </div>
 
-        <section id ="valores" class="valor">
+        <div id="valores">
             <h2>Valores</h2>
             <img src="GIF/planetsVal_.gif" alt="">
-        </section>
-
-        <section id="clientes">
-            <h2>Clientes</h2>
-            <img src="Img/clientes_.gif" alt="">
-        </section>
+            
+        </div>
+        <div id="final">
+            <div id = "clientes">
+                <img src="Img/clientes_.gif" alt="">
+            </div>
+            <iframe id="mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.6871448073616!2d-98.21527652409351!3d19.077488151924463!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cfc14836897bf5%3A0x8e37c6811a1805aa!2sRoble%2072%2C%20Jes%C3%BAs%20Gonz%C3%A1lez%20Ortega%2C%2072040%20Puebla%2C%20Pue.!5e0!3m2!1sen!2smx!4v1682402513641!5m2!1sen!2smx" style="border:0; border-radius:15px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>    
+        </div>
+           
 
                                 
     </main>
@@ -319,27 +324,27 @@
 </body>
 
 <footer>
-<div class="Interes">
-<ul>
-    <h4>Paginas</h4>
-    <li>Hola</li>
-    <li>ADIOS</li>
-</ul>
-</div>
-<div class="contactos">
-<ul>
-    <h4>Contactanos</h4>
-    <li>Hola</li>
-    <li>ADIOS</li>
-</ul>
-</div>
-<div class="direccion">
-<ul>
-    <h4>Dirrecion</h4>
-    <li>Hola</li>
-    <li>ADIOS</li>
-</ul>
-</div>
+    <div class="Interes">
+    <ul>
+        <h4>Paginas</h4>
+        <li>Hola</li>
+        <li>ADIOS</li>
+    </ul>
+    </div>
+    <div class="contactos">
+    <ul>
+        <h4>Contactanos</h4>
+        <li>Hola</li>
+        <li>ADIOS</li>
+    </ul>
+    </div>
+    <div class="direccion">
+    <ul>
+        <h4>Dirrecion</h4>
+        <li>Hola</li>
+        <li>ADIOS</li>
+    </ul>
+    </div>
 </footer>
 <div class="foo">2023 @CISTEAM</div>
 <script type="text/javascript">
